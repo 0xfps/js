ce499ca9a7da08fed8c09327c26c6ab423062b25
fps <anthony.nnaemeka.umeh@gmail.com>
2022-10-22 12:19:37 +0100

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+// Introduction to variables.
+console.log(5 + 6)
+
+// var, variable, changeable.
+// const, constant, unchangeable.
+// let, variable, changeable.
+
+const a = 5;
+
+var j = 10
+j = a
+let myVar = 8
+myVar = 18
+
+console.log(myVar ** 2)
+console.log(a)
