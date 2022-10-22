d64a7e4623042899408e5065d5ff1681d750bbcb
fps <anthony.nnaemeka.umeh@gmail.com>
2022-10-22 15:04:23 +0100

    Add files via upload

--- js/variables.js
+++ js/variables.js
@@ -7,10 +7,25 @@ console.log(5 + 6)
 
 const a = 5;
 
-var j = 10
-j = a
+var $j = 10
+$j = a
 let myVar = 8
 myVar = 18
 
+variableDeclared = 15
+
+const name = "John", sex = "male", age = 15
+
 console.log(myVar ** 2)
-console.log(a)
+console.log($j)
+console.log(variableDeclared)
+
+console.log(`${name} is ${age} years old, and is a ${sex}`)
+
+console.log("5" + 3 + 2)
+console.log(5 + 3 + "4")
+/* Bringing a string into an int fight, changes everything to string starting from
+	the point the string was introduces.
+	Line 25, concat starts at "5". >>> Output:: 532.
+	LIke 26, concat starts at "4". >>> Output:: 84.
+*/
