df7e34a8ab6fef5476b1fe635237776cc7eacfcf
fps <anthony.nnaemeka.umeh@gmail.com>
2022-10-24 02:11:26 -0700

    Add files via upload

--- object.js
+++ object.js
@@ -6,3 +6,17 @@ const Motor = {
 }
 
 Motor.start()
+
+function Person (fname, lname) {
+	this.firstname = fname
+	this.lastname = lname
+
+	this.age = 10
+
+	this.height = (x) => {
+		console.log("Height is " + x)
+	}
+}
+
+const person = new Person("A", "B")
+person.height(10)
